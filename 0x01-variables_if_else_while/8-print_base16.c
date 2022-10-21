9f5d955417272f439add0256e62689c59f438408
Marriamsaein <marriamsaein58@gmail.com>
2022-10-21 01:07:04 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ *
+ * Return: Always (0) Success
+ */
+
+int main(void)
+{
+	int i;
+	char ch;
+
+	for (i = '0'; i <= '9'; ++i)
+		putchar (i);
+	for (ch = 'a'; ch <= 'f'; ++ch)
+		putchar (ch);
+	putchar ('\n');
+
+	return (0);
+}
